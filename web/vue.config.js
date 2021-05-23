087acf7c871fcd5318c6fdd24548e5495e3b483f
Andrew Smirnov <smirnov@informatics.ru>
2021-05-23 13:07:58 +0300

    Fix css path dist

--- web/vue.config.js
+++ web/vue.config.js
@@ -2,5 +2,5 @@ module.exports = {
   transpileDependencies: [
     'vuetify'
   ],
-  publicPath: process.env.NODE_ENV === 'production' ? '/vk_graph/' : '/'
+  publicPath: process.env.NODE_ENV === 'production' ? '/dist/' : '/'
 }
