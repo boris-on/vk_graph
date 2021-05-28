af4c8a9f4c9afe449353e683f3ed7fe01ed83b47
boris yeltsin <xlboriska2015@gmail.com>
2021-05-28 17:37:06 +0300

    remade rep

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,76 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-app-bar color="#000F11" dark flat>
+      <v-row align="center">
+        <v-col class="pl-6">
+          <v-row align="center">
+            <v-img src="logo.png" max-height="40" max-width="40"></v-img>
+            <v-toolbar-title class="ml-3">VK Audience Graph</v-toolbar-title>
+          </v-row>
+        </v-col>
+
+        <v-spacer></v-spacer>
+
+        <v-col class="text-right">
+          <v-tooltip bottom transition="None">
+            <template v-slot:activator="{ on, attrs }">
+              <v-hover v-slot="{ hover }" open-delay="0" close-delay="0">
+                <v-icon
+                  class="mr-8"
+                  v-bind="attrs"
+                  v-on="on"
+                  size="big"
+                  :color="hover ? 'rgb(90, 90, 255)' : 'rgb(180, 180, 180)'"
+                >
+                  mdi-cog-outline
+                </v-icon>
+              </v-hover>
+            </template>
+            <span>Settings</span>
+          </v-tooltip>
+
+          <v-tooltip class="pr-7" bottom transition="None">
+            <template v-slot:activator="{ on, attrs }">
+              <v-hover v-slot="{ hover }" open-delay="0" close-delay="0">
+				<a href="/build_your_graph">
+                <v-icon
+                  class="mr-8"
+                  v-bind="attrs"
+                  v-on="on"
+                  size="big"
+                  :color="hover ? 'rgb(90, 90, 255)' : 'rgb(180, 180, 180)'"
+                >
+                  mdi-account-group
+                </v-icon></a>
+              </v-hover>
+            </template>
+            <span>Community</span>
+          </v-tooltip>
+        </v-col>
+      </v-row>
+    </v-app-bar>
+
+    <v-main>
+      <!-- <ImgGraph /> -->
+      <router-view />
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import ImgGraph from "./components/ImgGraph";
+// import BasicGraph from "./components/BasicGraph";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+  name: "App",
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  components: {
+    // ImgGraph,
+    // BasicGraph,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,32 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+// import vuetify from "./plugins/vuetify";
+import ForceGraph3D from "3d-force-graph";
+import Three from "three";
+import VueRouter from "vue-router";
+import BasicGraph from "./components/BasicGraph"
 
-Vue.config.productionTip = false
+
+Vue.config.productionTip = false;
+Vue.use(VueRouter);
+
+const routes = [
+	{
+		path: "",
+		component: BasicGraph,
+	},
+];
+
+const router = new VueRouter({
+	routes,
+	mode: "history",
+});
+
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+	// vuetify,
+	Three,
+	ForceGraph3D,
+	router,
+	render: (h) => h(App),
+}).$mount("#app");
